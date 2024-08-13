6c60659bd012517951cc157776b7541c03194e3c
gabiscalize <159174088+gabiscalize@users.noreply.github.com>
2024-08-13 13:46:13 -0300

    Add files via upload

---
+++ dump.sqlite (4).sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE alunos(
+  id int(3) unique,
+  mome varchar(50) NOT NULL,
+  idade varchar(2),
+  turma varchar(4)
+  );
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
